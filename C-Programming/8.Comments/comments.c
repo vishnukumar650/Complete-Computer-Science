0d480c6b44925f17977021797ea50f13ad9a0728
Vishnu Kumar <vishnukumar650@gmail.com>
2021-01-13 09:14:39 -0500

    C Programming 6 (Constants)

---
+++ C-Programming/8.Comments/comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*  Author: Vishnu Kumar 
+               This
+                is
+                a
+            Multi-line
+              Comment
+                                */
+    // I am a Single-line Comment
+    printf("Comments are fun...!!!\n"); // Again it's me, single-line comment
+
+    return 0;
+}
---
+++ C-Programming/9.Constants/constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const float PI = 3.141592653589793238; // constant variable named PI
+    // PI = 3.14 - error as constant variable cannot be modified
+    printf("Hello Constants\n"); // It can also be called constants, since it cannot be modified
+
+    printf("%d\n", 97); // Here the number 97 is a Constant, as it cannot be modified
+
+    return 0;
+}
