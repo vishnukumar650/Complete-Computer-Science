bf4070b625b8e8eadc2e9e271ce26c01b2ec4b15
Vishnu Kumar <vishnukumar650@gmail.com>
2021-01-13 05:29:51 -0500

    C Programming 3 (Data Types)

---
+++ C-Programming/5.Data_Types/data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 25;                                            // integer variable
+    float gpa = 3.5;                                         // floating point variable
+    char grade = 'A';                                        // character variable
+    double pi = 3.141592;                                    // double variable (long format of float)
+    char intro[] = "I am Vishnu Kumar (aka vishnukumar650)"; // character array (sometimes known as string)
+
+    return 0;
+}
