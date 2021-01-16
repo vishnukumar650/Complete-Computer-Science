99a106baf7819d956788544f60faf8a2fc8af3d0
Vishnu Kumar <vishnukumar650@gmail.com>
2021-01-15 22:27:52 -0500

    C Programming 17 (Guessing Game)

---
+++ C-Programming/20.Guessing_Game/guessing_game.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int secretNumber = 11;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
+
+    while (guess != secretNumber && outOfGuesses == 0)
+    {
+        if (guessCount < guessLimit)
+        {
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        }
+        else
+        {
+            outOfGuesses = 1;
+        }
+    }
+
+    if (outOfGuesses == 1)
+    {
+        printf("Out of guesses\n");
+    }
+    else
+    {
+        printf("You Win!\n");
+    }
+
+    return 0;
+}
