0ab9e6a5519b4be93982aa41d9b2f9c18e012e0d
Vishnu Kumar <vishnukumar650@gmail.com>
2021-01-22 22:11:49 -0500

    C Programming 26 (Ternary Operator)

---
+++ C-Programming/15.Operators/Ternary/ternary_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m = 5, n = 4;
+
+    (m > n) ? printf("m is greater than n that is %d > %d\n", m, n) : printf("n is greater than m that is %d > %d\n", n, m);
+
+    return 0;
+}
